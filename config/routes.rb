2b44e4a9c59e170ef415acf4d57f8764b01a95f6
Ivan Piskun <ivan.piskun@softswiss.com>
2022-05-21 15:24:58 +0300

    Add root controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   devise_for :doctors
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root "home#welcome"
+
+
+  match '*path' => redirect('/'), via: :get
+
 end
